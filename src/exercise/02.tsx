bd987c210977a6485e67f0e0760f29a89a39ad35
SEVRIN Yannick <yannicksevrin0@gmail.com>
2025-02-11 20:31:08 +0400

    Exercice 2 done

--- src/exercise/02.tsx
+++ src/exercise/02.tsx
@@ -4,31 +4,36 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Créer une variable `names` mais sans initiliser de valeurs
-//  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
-let names = []
+  // 🐶 Créer une variable `names` mais sans initiliser de valeurs
+  //  ajoute ensuite 'John' et 'doe' à `name` sans utiliser la méthode `push()`
+  let names: string[] = []
+  names = ["John", "Doe"]
 
-// 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
+  // 🤖 affiche 'Il y a 2 noms' dans le navigateur grace à `displayText` et `.length`
+  displayText(`il y a ${names.length} noms`)
 
-// 🐶 Créer une constante `games` et initialise avec un tableau vide
-// avec la méthode `push()` ajoute 3 jeux
-// ⛏️ const games
-// 🤖 affiche le nombre d'élément à l'ecran
+  // 🐶 Créer une constante `games` et initialise avec un tableau vide
+  // avec la méthode `push()` ajoute 3 jeux
+  // ⛏️ const games
+  const games: string[] = []
+  games.push("Minecraft", "Pokemon", "Tetris")
+  // 🤖 affiche le nombre d'élément à l'ecran
+  displayText(`il y a ${games.length} jeux`)
+  // 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
+  // ⛏️ const things
+  const things: any[] = []
 
-// 🐶 Créer une constante `things` : un tableau qui peut prendre n'importe quel type de donnée
-// ⛏️ const things
-
-// 🤖 affiche le nombre d'élément à l'ecran
-
-/*
+  // 🤖 affiche le nombre d'élément à l'ecran
+  displayText(`il y a ${things.length} autres`)
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
