d20aab540449b4022664886f81bf647d3275f7d7
SEVRIN Yannick <yannicksevrin0@gmail.com>
2025-02-12 20:44:59 +0400

    Exercice 4 done + bonus

--- src/exercise/04.tsx
+++ src/exercise/04.tsx
@@ -4,62 +4,72 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Déclare un type `pinType` qui permet d'avoir un number ou string
-// affecte ce type à la variable `pin`
+  // 🐶 Déclare un type `pinType` qui permet d'avoir un number ou string
+  // affecte ce type à la variable `pin`
+  type pinType = number | string
 
-let pin: number | string // ⛏️ remplace cette declaration
-pin = 0
-displayText(`Le pin est ${pin}`)
-pin = '10'
-displayText(`Le pin est ${pin}`)
+  let pin: pinType // ⛏️ remplace cette declaration
+  pin = 0
+  displayText(`Le pin est ${pin}`)
+  pin = "10"
+  displayText(`Le pin est ${pin}`)
 
-// 🐶 Déclare un type `primitives` qui permet d'avoir tous les types primitif number | boolean | string
+  // 🐶 Déclare un type `primitives` qui permet d'avoir tous les types primitif number | boolean | string
+  type primitives = number | boolean | string
+  // ⛏️ remplace `any` par le type `primitives` et affecte une bonne valeur
+  let prim: primitives = 12
+  displayText(`prim vaut  ${prim}`)
 
-// ⛏️ remplace `any` par le type `primitives` et affecte une bonne valeur
-let prim: any = {}
-displayText(`prim vaut  ${prim}`)
+  // 🐶 modifie le type `primitivesNullUndefined` pour que les lignes suivantes compilent
+  type primitivesNullUndefined = number | boolean | string | null | undefined
 
-// 🐶 modifie le type `primitivesNullUndefined` pour que les lignes suivantes compilent
-type primitivesNullUndefined = number | boolean | string
+  // ⛏️ décommente le code ci-dessous et fait en sorte que la compilation fonctionne en modifiant `primitivesNullUndefined`
+  let prim2: primitivesNullUndefined
+  displayText(`prim2 vaut  ${prim2}`)
 
-// ⛏️ décommente le code ci-dessous et fait en sorte que la compilation fonctionne en modifiant `primitivesNullUndefined`
-// let prim2: primitivesNullUndefined
-// displayText(`prim2 vaut  ${prim2}`)
+  // 🐶 Modifie le type `Person` avec les propiétés
+  // - name de type string
+  // - age de type number
+  // - isActive de type boolean
+  // - roles un tableau de string
+  // - params un object non defini
+  // - payload peut avoir n'importe quel type
 
-// 🐶 Modifie le type `Person` avec les propiétés
-// - name de type string
-// - age de type number
-// - isActive de type boolean
-// - roles un tableau de string
-// - params un object non defini
-// - payload peut avoir n'importe quel type
+  type Person = {
+    name: string
+    age: number
+    isActive: boolean
+    roles: string[]
+    params: {}
+    payload: any
+    friend: Person | undefined
+  }
 
-type Person = any
+  const person: Person = {
+    name: "John",
+    age: 23,
+    isActive: true,
+    roles: ["admin"],
+    params: { id: "50" },
+    payload: undefined,
+    friend: undefined,
+  }
 
-const person: Person = {
-  name: 'John',
-  age: 23,
-  isActive: true,
-  roles: ['admin'],
-  params: {id: '50'},
-  payload: undefined,
-}
+  displayText(
+    `${person.name} a ${person.age} ans et a le role ${person.roles[0]} `
+  )
 
-displayText(
-  `${person.name} a ${person.age} ans et a le role ${person.roles[0]} `,
-)
-
-// 🐶 N'oublie pas l'exercice bonus
-
-/*
+  // 🐶 N'oublie pas l'exercice bonus
+  displayText(`${person.name} a pour ami : ${person.friend?.name ?? "aucun"} `)
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
