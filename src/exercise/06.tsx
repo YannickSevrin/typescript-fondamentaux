a7c63a1b3f56f71c3e55754cd78fe31daeb688e4
SEVRIN Yannick <yannicksevrin0@gmail.com>
2025-02-20 18:31:59 +0400

    Module 1 - done 👌

--- src/exercise/06.tsx
+++ src/exercise/06.tsx
@@ -4,36 +4,83 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Déclare un type `Connexion` un tuple contennant toutes les informations utile a la connexion
-// - le nom de la connexion  (ex : Connexion à Google) `string`
-// - le protocole (ex : ftp, http) `string`
-// - le hostname (ex : google.com) `string`
-// - le port  (ex : 423) `number`
-// - le username : (ex : admin) `string`
-// - le password (ex : admin) `string`
-// 📝 documentation tuples https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+  // 🐶 Déclare un type `Connexion` un tuple contennant toutes les informations utile a la connexion
 
-// type Connexion
+  // - le nom de la connexion  (ex : Connexion à Google) `string`
+  // - le protocole (ex : ftp, http) `string`
+  // - le hostname (ex : google.com) `string`
+  // - le port  (ex : 423) `number`
+  // - le username : (ex : admin) `string`
+  // - le password (ex : admin) `string`
+  // 📝 documentation tuples https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+  enum Protocol {
+    HTTP = "http",
+    HTTPS = "https",
+    FTP = "ftp",
+  }
+  // type Connexion
+  type Connexion = [
+    nom: string,
+    protocole: Protocol,
+    hostname: string,
+    port: number,
+    username: string,
+    password: string,
+  ]
 
-// 🐶 Créer une variable 'google' qui contiendra les infos de connexion à google
-// https google.com 443
-// Affiche le resultat à l'ecran avec le code
-// 🤖 displayText(`Connexion ${google[0]} : ${google[1]}://${google[2]}:${google[3]} `)
+  // 🐶 Créer une variable 'google' qui contiendra les infos de connexion à google
+  const google: Connexion = [
+    "google",
+    Protocol.HTTPS,
+    "google.com",
+    423,
+    "admin",
+    "admin",
+  ]
 
-// 🐶 créer 3 autres connexions de ton choix et affiche le resultat à l'écran
+  const gmail: Connexion = [
+    "gmail",
+    Protocol.HTTP,
+    "gmail.com",
+    423,
+    "admin",
+    "admin",
+  ]
 
-// 🐶 créer un tableau `connexions` qui contiendra toutes les connexions
-// Ajoute des connexion via l'initialisation et via la méthode `push`
+  const ftp: Connexion = ["ftp", Protocol.FTP, "ftp.com", 423, "admin", "admin"]
 
-/*
+  const mikecodeur: Connexion = [
+    "mikecodeur",
+    Protocol.HTTP,
+    "mikecodeur.com",
+    423,
+    "admin",
+    "admin",
+  ]
+  // https google.com 443
+  // Affiche le resultat à l'ecran avec le code
+  displayText(
+    `Connexion ${google[0]} : ${google[1]}://${google[2]}:${google[3]} `
+  )
+
+  // 🐶 créer 3 autres connexions de ton choix et affiche le resultat à l'écran
+
+  // 🐶 créer un tableau `connexions` qui contiendra toutes les connexions
+  // Ajoute des connexion via l'initialisation et via la méthode `push`
+  const connexions: Connexion[] = [google, gmail]
+  connexions.push(ftp, mikecodeur)
+
+  let [, gmailProtocol, gmailHostname] = gmail
+  displayText(`Connexion ${gmailProtocol} : ${gmailHostname} `)
+  /*
 eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
--- src/exercise/09.tsx
+++ src/exercise/09.tsx
@@ -4,76 +4,105 @@
 
 // ❌ NE PAS MODIFIER
 // Utilitaire ne faisant pas partie de l'exercice
-import displayText, { App, init } from "../lib/exerciseHelper"; 
+import displayText, { App, init } from "../lib/exerciseHelper"
 const exercice = () => {
-init()
+  init()
 
-// ✔️ Début de l'exercice
+  // ✔️ Début de l'exercice
 
-// 🐶 Regarde le code existant et sous le Class 'Cat' créé la classe 'Dog'
-interface IName {
-  name: string
-  printName: () => string
-}
-interface Runnable {
-  run: () => void
-}
-interface Drinkable {
-  drink: () => void
-}
-
-class Animal implements IName {
-  // name: string | undefined
-  nbLeg: number
-  underwater: boolean
-  constructor(name: string, nbLeg: number, underwater: boolean) {
-    this.name = name
-    this.nbLeg = nbLeg
-    this.underwater = underwater
+  // 🐶 Regarde le code existant et sous le Class 'Cat' créé la classe 'Dog'
+  interface IName {
+    name: string
+    printName: () => string
   }
-  name: string
-  printName() {
-    console.log(`Mon nom est ${this.name}`)
-    return this.name
+  interface Runnable {
+    run: () => void
   }
-}
-
-class Cat extends Animal implements Runnable, Drinkable {
-  constructor(name: string) {
-    super(name, 4, false)
+  interface Drinkable {
+    drink: () => void
   }
-  run() {
-    console.log(`Je cours`)
+  // interface Mammal extends Runnable, Drinkable {}
+  type Mammal = Runnable & Drinkable
+  class Animal implements IName {
+    // name: string | undefined
+    nbLeg: number
+    underwater: boolean
+    constructor(name: string, nbLeg: number, underwater: boolean) {
+      this.name = name
+      this.nbLeg = nbLeg
+      this.underwater = underwater
+    }
+    name: string
+    printName() {
+      console.log(`Mon nom est ${this.name}`)
+      return this.name
+    }
   }
-  drink() {
-    console.log(`Je bois`)
+
+  class Cat extends Animal implements Mammal {
+    constructor(name: string) {
+      super(name, 4, false)
+    }
+    run() {
+      console.log(`Je cours`)
+    }
+    drink() {
+      console.log(`Je bois`)
+    }
   }
-}
 
-// 🐶 Implemente correctement la classe 'Dog'
-// ⛏️ Décommente la classe 'dog' ci-dessous et constate le message d'erreur
-//class Dog extends Animal implements Runnable, Drinkable {
-// 🐶 implétmente correctement dog
-//}
+  // 🐶 Implemente correctement la classe 'Dog'
+  // ⛏️ Décommente la classe 'dog' ci-dessous et constate le message d'erreur
+  class Dog extends Animal implements Mammal {
+    // 🐶 implétmente correctement dog
+    constructor(name: string) {
+      super(name, 4, false)
+    }
+    run() {
+      console.log(`Je cours`)
+    }
+    drink() {
+      console.log(`Je bois`)
+    }
+  }
 
-// 🐶 Créé une interface 'Swimable' contenant la fonction 'swim()'
+  // 🐶 Créé une interface 'Swimable' contenant la fonction 'swim()'
+  interface Swimable {
+    swim: () => void
+  }
+  // 🐶 adapte la classe Fish en étantdant 'annimal' et implementant 'Swimable'
+  class Fish extends Animal implements Swimable {
+    constructor(name: string) {
+      super(name, 0, true)
+    }
+    swim() {
+      console.log(`je nage`)
+    }
+  }
 
-// 🐶 adapte la classe Fish en étantdant 'annimal' et implementant 'Swimable'
-class Fish {}
+  const tigrou = new Cat("Tigrou")
+  tigrou.run()
+  tigrou.drink()
+  displayText(`Nom du chat ${tigrou.printName()}`)
+  const wouf = new Dog("Wouf")
+  wouf.run()
+  wouf.drink()
+  displayText(`Nom du chien ${wouf.printName()}`)
+  const plouf = new Fish("Plouf")
+  plouf.swim()
+  displayText(`Nom du poisson ${plouf.printName()}`)
 
-const tigrou = new Cat('Tigrou')
-tigrou.run()
-tigrou.drink()
-displayText(`Nom du chat ${tigrou.printName()}`)
+  // 🐶 Créé une instance de Dog et appelle les fonctions 'run' et 'drink'
+  // utilise 'displayText' pour afficher son nom à l'écran
 
-// 🐶 Créé une instance de Dog et appelle les fonctions 'run' et 'drink'
-// utilise 'displayText' pour afficher son nom à l'écran
-
-// 🐶 Créé une instance de Fish et appelle la fonction 'swim'
-// utilise 'displayText' pour afficher son nom à l'écran
-
-/*eslint
+  // 🐶 Créé une instance de Fish et appelle la fonction 'swim'
+  // utilise 'displayText' pour afficher son nom à l'écran
+  function doSomeThingWithDrinkers(drinker: Drinkable) {
+    drinker.drink()
+  }
+  // doSomeThingWithDrinkers(plouf)
+  /*eslint
   @typescript-eslint/no-unused-vars: "off"
 */
-};
-export default () => <App exercice={exercice} />;
+}
+export default () => <App exercice={exercice} />
